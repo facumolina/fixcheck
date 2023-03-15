52fe771a4c95529983d70112c491ef15a616f27d
Facundo Molina <facu.molina7@gmail.com>
2023-03-15 08:06:05 +0100

    Minor progress in main class

--- src/main/java/org/imdea/fixcheck/FixCheck.java
+++ src/main/java/org/imdea/fixcheck/FixCheck.java
@@ -1,7 +1,20 @@
 package org.imdea.fixcheck;
 
+/**
+ * FixCheck class: main class.
+ *
+ * @author Facundo Molina <facundo.molina@imdea.org>
+ */
 public class FixCheck {
     public static void main(String[] args) {
         System.out.println("> FixCheck");
+        String bugRevealingTestClass = args[0];
+        System.out.println("bugRevealingTestClass: " + bugRevealingTestClass);
+
+
+    }
+
+    public static void loadBugRevealingTest(String bugRevealingTestClass) {
+
     }
 }
