e21268bc0deaeee98b35ad5406207d9e54e1c0df
Facundo Molina <facu.molina7@gmail.com>
2023-03-15 08:50:20 +0100

    Tests for load prefix

--- src/test/java/org/imdea/fixcheck/loader/TestLoaderTests.java
+++ src/test/java/org/imdea/fixcheck/loader/TestLoaderTests.java
@@ -1,10 +1,13 @@
 package org.imdea.fixcheck.loader;
 
+import org.imdea.fixcheck.prefix.Prefix;
 import org.junit.Test;
 import sootup.core.model.SootClass;
 import sootup.java.core.JavaSootClassSource;
 
-import static org.junit.Assert.assertNotNull;
+import java.util.List;
+
+import static org.junit.Assert.*;
 
 /**
  * TestLoaderTests class: tests for TestLoader.
@@ -15,10 +18,21 @@ public class TestLoaderTests {
 
   @Test
   public void testLoadTestClass() {
+    String classesPath = "/Users/facundo.molina/research/software/bad-fixes-dataset/tmp/jackson-databind-118/badfix/jackson-databind/target/test-classes";
     String testClass = "com.fasterxml.jackson.databind.jsontype.TestExternalIdForAnalysis";
-    TestLoader.setup("/Users/facundo.molina/research/software/bad-fixes-dataset/tmp/jackson-databind-118/badfix/jackson-databind/target/test-classes");
+    TestLoader.setup(classesPath);
     SootClass<JavaSootClassSource> sootClass = TestLoader.loadTestClass(testClass);
     assertNotNull(sootClass);
+  }
+
+  @Test
+  public void testLoadPrefixes() {
+    String classesPath = "/Users/facundo.molina/research/software/bad-fixes-dataset/tmp/jackson-databind-118/badfix/jackson-databind/target/test-classes";
+    String testClass = "com.fasterxml.jackson.databind.jsontype.TestExternalIdForAnalysis";
+    TestLoader.setup(classesPath);
+    List<Prefix> prefixes = TestLoader.loadPrefixes(classesPath, testClass);
+    assertNotNull(prefixes);
+    assertEquals(1, prefixes.size());
   }
 
 }
