beb221a56c78ab1927517a0426e4695b7fe6f58c
Facundo Molina <facu.molina7@gmail.com>
2023-03-15 08:50:57 +0100

    Main file using and loading prefies

--- src/main/java/org/imdea/fixcheck/FixCheck.java
+++ src/main/java/org/imdea/fixcheck/FixCheck.java
@@ -1,20 +1,27 @@
 package org.imdea.fixcheck;
 
+import org.imdea.fixcheck.loader.TestLoader;
+import org.imdea.fixcheck.prefix.Prefix;
+
+import java.util.List;
+
 /**
  * FixCheck class: main class.
  *
  * @author Facundo Molina <facundo.molina@imdea.org>
  */
 public class FixCheck {
-    public static void main(String[] args) {
-        System.out.println("> FixCheck");
-        String bugRevealingTestClass = args[0];
-        System.out.println("bugRevealingTestClass: " + bugRevealingTestClass);
+  public static void main(String[] args) {
+    System.out.println("> FixCheck");
+    String targetTestsPath = args[0];
+    String targetTests = args[1];
+    System.out.println("target tests path: " + targetTestsPath);
+    System.out.println("bug revealing tests: " + targetTests);
+    System.out.println();
 
+    // Loading the prefixes to analyze
+    List<Prefix> prefixes = TestLoader.loadPrefixes(targetTestsPath, targetTests);
+    System.out.println("prefixes: " + prefixes.size());
+  }
 
-    }
-
-    public static void loadBugRevealingTest(String bugRevealingTestClass) {
-
-    }
 }
