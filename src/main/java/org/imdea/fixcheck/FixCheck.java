372af11cd16addaa7d8465603844bd10772e3ece
Facundo Molina <facu.molina7@gmail.com>
2023-03-15 17:30:00 +0100

    Adapt prefixes creation to the old soot

--- src/main/java/org/imdea/fixcheck/FixCheck.java
+++ src/main/java/org/imdea/fixcheck/FixCheck.java
@@ -2,11 +2,14 @@ package org.imdea.fixcheck;
 
 import org.imdea.fixcheck.loader.TestLoader;
 import org.imdea.fixcheck.prefix.Prefix;
-import soot.Body;
-import soot.SootClass;
-import soot.SootMethod;
+import soot.*;
+import soot.jimple.JasminClass;
+import soot.jimple.Jimple;
+import soot.jimple.JimpleBody;
+import soot.options.Options;
+import soot.util.JasminOutputStream;
 
-import java.io.OutputStream;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -33,7 +36,7 @@ public class FixCheck {
     // TODO: first approach: generate similar prefixes by changing the 'inputs' in the given prefixes
   }
 
-  public static List<Prefix> generateSimilarPrefixes(List<Prefix> prefixes, int n) throws ClassNotFoundException {
+  public static List<Prefix> generateSimilarPrefixes(List<Prefix> prefixes, int n) throws ClassNotFoundException, IOException {
     String inputClass = "java.util.Date";
     String inputType = "java.lang.Object";
     Class<?> inputClassType = Class.forName(inputType);
@@ -41,8 +44,34 @@ public class FixCheck {
     for (Prefix prefix : prefixes) {
       SootMethod method = prefix.getMethod();
       SootClass sootClass = prefix.getMethodClass();
+      Body oldBody = method.retrieveActiveBody();
+
+
       for (int i=0; i < n; i++) {
         // Generate n similar prefixes
+        SootMethod newMethod = new SootMethod("similarPrefix"+i, method.getParameterTypes(), method.getReturnType(), method.getModifiers());
+
+        sootClass.addMethod(newMethod);
+        // Create the method body
+        Body newBody = (Body)oldBody.clone();
+        newMethod.setActiveBody(newBody);
+
+        for (SootMethod m : sootClass.getMethods()) {
+          m.retrieveActiveBody();
+        }
+
+        String fileName = SourceLocator.v().getFileNameFor(sootClass, Options.output_format_class);
+        File f = new File(fileName);
+        f.getParentFile().mkdirs();
+        f.createNewFile(); // if file already exists will do nothing
+        OutputStream streamOut = new JasminOutputStream(
+            new FileOutputStream(f, false));
+        PrintWriter writerOut = new PrintWriter(
+            new OutputStreamWriter(streamOut));
+        JasminClass jasminClass = new soot.jimple.JasminClass(sootClass);
+        jasminClass.print(writerOut);
+        writerOut.flush();
+        streamOut.close();
 
         break;
       }
