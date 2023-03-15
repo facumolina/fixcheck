ecf3e2cc4728d71604205e54a44226943234e822
Facundo Molina <facu.molina7@gmail.com>
2023-03-15 08:49:57 +0100

    TestLoader allows to load the prefixes

--- src/main/java/org/imdea/fixcheck/loader/TestLoader.java
+++ src/main/java/org/imdea/fixcheck/loader/TestLoader.java
@@ -2,10 +2,15 @@ package org.imdea.fixcheck.loader;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.imdea.fixcheck.prefix.Prefix;
 import sootup.core.Language;
 import sootup.core.Project;
 import sootup.core.inputlocation.AnalysisInputLocation;
 import sootup.core.model.SootClass;
+import sootup.core.model.SootMethod;
 import sootup.core.types.ClassType;
 import sootup.core.views.View;
 import sootup.java.bytecode.inputlocation.PathBasedAnalysisInputLocation;
@@ -39,9 +44,8 @@ public class TestLoader {
 
   /**
    * Load a test class.
-   *
-   * @param testClass is the name of the test class to load
-   * @return the loaded test class as a SootClass
+   * @param testClass is the name of the test class to load.
+   * @return the loaded test class as a SootClass.
    */
   public static SootClass<JavaSootClassSource> loadTestClass(String testClass) {
     System.out.println("Loading test class: " + testClass);
@@ -49,6 +53,23 @@ public class TestLoader {
     if (!projectView.getClass(classType).isPresent())
       throw new IllegalArgumentException("The class " + testClass + " is not present in the project. Is the path to the binary correct?");
     return  (SootClass<JavaSootClassSource>) projectView.getClass(classType).get();
+  }
+
+  /**
+   * Load prefixes from the given test class.
+   * @param testClass is the name of the test class to load the prefixes from.
+   * @return the list of prefixes.
+   */
+  public static List<Prefix> loadPrefixes(String classesPath, String testClass) {
+    List<Prefix> prefixes = new ArrayList<>();
+    if (project == null || projectView == null)
+      setup(classesPath);
+    SootClass<JavaSootClassSource> sootClass = loadTestClass(testClass);
+    for (SootMethod method : sootClass.getMethods()) {
+      if (method.getName().equals("<init>")) continue;
+      prefixes.add(new Prefix(method));
+    }
+    return prefixes;
   }
 
 }
