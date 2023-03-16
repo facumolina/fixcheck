de0ae258531722c21a3afb1ebd8dcb498313c71c
Facundo Molina <facu.molina7@gmail.com>
2023-03-16 11:25:18 +0100

    Adding first version of InputTransformer

---
+++ src/main/java/org/imdea/fixcheck/transform/input/InputTransformer.java
@@ -0,0 +1,39 @@
+package org.imdea.fixcheck.transform.input;
+
+import org.imdea.fixcheck.prefix.Prefix;
+import org.imdea.fixcheck.transform.Initializer;
+import org.imdea.fixcheck.transform.PrefixTransformer;
+import soot.Body;
+import soot.SootClass;
+import soot.SootMethod;
+
+/**
+ * Input Transformer class: transform a prefix by changing its 'input'.
+ * @author Facundo Molina
+ */
+public class InputTransformer extends PrefixTransformer {
+
+  public InputTransformer(Prefix prefix) {
+    super(prefix);
+  }
+
+  @Override
+  public Prefix transform() {
+    Prefix prefix = this.prefix;
+    SootClass prefixClass = prefix.getMethodClass();
+    SootMethod prefixMethod = prefix.getMethod();
+    Body oldBody = prefixMethod.retrieveActiveBody();
+
+    System.out.println("Generating similar prefix with InputTransformer");
+    SootClass newClass = Initializer.initializeTransformedClass("SimilarPrefixClass",prefixClass);
+    SootMethod newMethod = new SootMethod("similarPrefix", prefixMethod.getParameterTypes(), prefixMethod.getReturnType(), prefixMethod.getModifiers());
+    newMethod.addAllTagsOf(prefixMethod);
+    newClass.addMethod(newMethod);
+    Body newBody = (Body)oldBody.clone();
+    newMethod.setActiveBody(newBody);
+    newClass.addMethod(newMethod);
+
+    return new Prefix(newMethod, newClass);
+  }
+
+}
