23c68593732f36c2451cceb08bf5e8e782782174
Facundo Molina <facu.molina7@gmail.com>
2023-03-17 17:23:53 +0100

    Very simple AssertFalseGenerator is working and inserting a statement that causes the prefix execution to fail

--- src/main/java/org/imdea/fixcheck/assertion/AssertFalseGenerator.java
+++ src/main/java/org/imdea/fixcheck/assertion/AssertFalseGenerator.java
@@ -2,6 +2,7 @@ package org.imdea.fixcheck.assertion;
 
 import org.imdea.fixcheck.prefix.Prefix;
 import soot.*;
+import soot.jimple.IntConstant;
 import soot.jimple.Jimple;
 import soot.jimple.parser.node.TBoolConstant;
 
@@ -21,14 +22,17 @@ public class AssertFalseGenerator extends AssertionGenerator {
       SootMethod method = prefix.getMethod();
       Body body = method.retrieveActiveBody();
       UnitPatchingChain units = body.getUnits();
-      SootClass cl = Scene.v().loadClassAndSupport("org.junit.Assert");
-      System.out.println("methods: "+ cl.getMethods());
+      SootClass cl = Scene.v().getSootClass("org.junit.Assert");
       // Define local for org.junit.Assert
       Local assertRef = Jimple.v().newLocal("assertRef", RefType.v("org.junit.Assert"));
       body.getLocals().add(assertRef);
       // Add the assertFalse at the end of the prefix
       SootMethod assertMethod = Scene.v().getMethod("<org.junit.Assert: void assertFalse(boolean)>");
       //units.addLast(Jimple.v().newInvokeStmt(Jimple.v().newVirtualInvokeExpr(assertRef, assertMethod.makeRef(), TBoolConstant)
+      Unit unit = Jimple.v().newInvokeStmt(Jimple.v().newStaticInvokeExpr(assertMethod.makeRef(), IntConstant.v(1)));
+      units.removeLast(); // Remove the return statement
+      units.addLast(unit);
+      units.addLast(Jimple.v().newReturnVoidStmt());
     }
 
 }
