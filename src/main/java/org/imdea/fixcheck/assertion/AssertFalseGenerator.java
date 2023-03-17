6204ac187e665098233035438ce2d7f6609633d1
Facundo Molina <facu.molina7@gmail.com>
2023-03-17 17:28:56 +0100

    Removing commented code and unused var

--- src/main/java/org/imdea/fixcheck/assertion/AssertFalseGenerator.java
+++ src/main/java/org/imdea/fixcheck/assertion/AssertFalseGenerator.java
@@ -4,7 +4,6 @@ import org.imdea.fixcheck.prefix.Prefix;
 import soot.*;
 import soot.jimple.IntConstant;
 import soot.jimple.Jimple;
-import soot.jimple.parser.node.TBoolConstant;
 
 /**
  * AssertFalseGenerator class: simple generator including an assert(false) at the end of the prefix, just for testing.
@@ -22,13 +21,11 @@ public class AssertFalseGenerator extends AssertionGenerator {
       SootMethod method = prefix.getMethod();
       Body body = method.retrieveActiveBody();
       UnitPatchingChain units = body.getUnits();
-      SootClass cl = Scene.v().getSootClass("org.junit.Assert");
       // Define local for org.junit.Assert
       Local assertRef = Jimple.v().newLocal("assertRef", RefType.v("org.junit.Assert"));
       body.getLocals().add(assertRef);
       // Add the assertFalse at the end of the prefix
       SootMethod assertMethod = Scene.v().getMethod("<org.junit.Assert: void assertFalse(boolean)>");
-      //units.addLast(Jimple.v().newInvokeStmt(Jimple.v().newVirtualInvokeExpr(assertRef, assertMethod.makeRef(), TBoolConstant)
       Unit unit = Jimple.v().newInvokeStmt(Jimple.v().newStaticInvokeExpr(assertMethod.makeRef(), IntConstant.v(1)));
       units.removeLast(); // Remove the return statement
       units.addLast(unit);
