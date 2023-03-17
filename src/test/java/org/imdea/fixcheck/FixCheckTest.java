29683547646277e6244416d9761ea108d84d7268
Facundo Molina <facu.molina7@gmail.com>
2023-03-17 11:49:00 +0100

    Full classpath including dependencies must be provided in order to soot be able to load all the classes under analysis

--- src/test/java/org/imdea/fixcheck/FixCheckTest.java
+++ src/test/java/org/imdea/fixcheck/FixCheckTest.java
@@ -4,6 +4,7 @@ import org.imdea.fixcheck.loader.TestLoader;
 import org.imdea.fixcheck.prefix.Prefix;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
@@ -14,9 +15,11 @@ public class FixCheckTest {
   @Test
   public void testSimilarPrefixGeneration() throws ClassNotFoundException, IOException {
     // Load the prefixes to analyze
-    String classesPath = "/Users/facundo.molina/research/software/bad-fixes-dataset/tmp/jackson-databind-118/badfix/jackson-databind/target/test-classes";
+    String testClassesPath = "/Users/facundo.molina/research/software/bad-fixes-dataset/tmp/jackson-databind-118/badfix/jackson-databind/target/test-classes";
+    String alldepspath = "build/libs/fixcheck-all-1.0.0.jar:/Users/facundo.molina/research/software/bad-fixes-dataset/tmp/jackson-databind-118/badfix/jackson-databind/target/jackson-databind-2.1.2-SNAPSHOT-jar-with-dependencies.jar";
+    String cp = alldepspath + ":" + testClassesPath;
     String testClass = "com.fasterxml.jackson.databind.jsontype.TestExternalIdForAnalysis";
-    List<Prefix> prefixes = TestLoader.loadPrefixes(classesPath, testClass);
+    List<Prefix> prefixes = TestLoader.loadPrefixes(cp, testClass);
     // Generate similar prefixes
     List<Prefix> similarPrefixes = FixCheck.generateSimilarPrefixes(prefixes, 10);
     assertEquals(0, similarPrefixes.size());
