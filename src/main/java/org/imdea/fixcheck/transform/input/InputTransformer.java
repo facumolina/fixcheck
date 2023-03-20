f553bd08bc92e3d9777ca185eb828a8c3df03264
Facundo Molina <facu.molina7@gmail.com>
2023-03-20 10:40:32 +0100

    First draft of code replacing an input

--- src/main/java/org/imdea/fixcheck/transform/input/InputTransformer.java
+++ src/main/java/org/imdea/fixcheck/transform/input/InputTransformer.java
@@ -3,9 +3,8 @@ package org.imdea.fixcheck.transform.input;
 import org.imdea.fixcheck.prefix.Prefix;
 import org.imdea.fixcheck.transform.Initializer;
 import org.imdea.fixcheck.transform.PrefixTransformer;
-import soot.Body;
-import soot.SootClass;
-import soot.SootMethod;
+import soot.*;
+import soot.jimple.*;
 
 /**
  * Input Transformer class: transform a prefix by changing its 'input'.
@@ -28,11 +27,70 @@ public class InputTransformer extends PrefixTransformer {
     SootClass newClass = Initializer.initializeTransformedClass(prefixClass.getPackageName() + ".SimilarPrefixClass", prefixClass);
     SootMethod newMethod = new SootMethod("similarPrefix", prefixMethod.getParameterTypes(), prefixMethod.getReturnType(), prefixMethod.getModifiers());
     newMethod.addAllTagsOf(prefixMethod);
+    newMethod.setExceptions(prefixMethod.getExceptions());
     Body newBody = (Body)oldBody.clone();
+    replaceInput(newBody);
     newMethod.setActiveBody(newBody);
     newClass.addMethod(newMethod);
 
     return new Prefix(newMethod, newClass);
+  }
+
+  private void replaceInput(Body body) {
+    Local input = getLocalWithType(body, "java.util.Date");
+    // Define local for the new input
+    Local newInput = Jimple.v().newLocal("newInput", RefType.v("java.lang.Integer"));
+    body.getLocals().add(newInput);
+    // Add call for new input constructor
+    AssignStmt assignStmt = Jimple.v().newAssignStmt(newInput, Jimple.v().newNewExpr(RefType.v("java.lang.Integer")));
+    SootMethod newInputConstructor = Scene.v().getMethod("<java.lang.Integer: void <init>(int)>");
+    InvokeStmt constructorInvoke = Jimple.v().newInvokeStmt(Jimple.v().newSpecialInvokeExpr(newInput, newInputConstructor.makeRef(), IntConstant.v(1)));
+    // Replace old input constructor with new input constructor
+    replaceConstructor(body, input, assignStmt, constructorInvoke);
+    // Use the new input in the right place
+    replace(body, input, newInput);
+  }
+
+  private Local getLocalWithType(Body body, String typeName) {
+    for (Local local : body.getLocals()) {
+      if (local.getType().toString().equals(typeName)) {
+        return local;
+      }
+    }
+    return null;
+  }
+  /* Replace all uses of v1 in body with v2 */
+  private void replace(Body body, Value v1, Value v2) {
+    for (Unit ut : body.getUnits()) {
+      for (ValueBox vb : ut.getUseBoxes())
+        if( vb.getValue().equals(v1))
+          vb.setValue(v2);
+    }
+  }
+
+  private void replaceConstructor(Body body, Local inputToReplace, AssignStmt assignStmt, InvokeStmt constructorInvoke) {
+    // First replace the assignment
+    for (Unit ut : body.getUnits()) {
+      if (ut instanceof AssignStmt) {
+        AssignStmt assign = (AssignStmt) ut;
+        if (assign.getLeftOp().equals(inputToReplace)) {
+          body.getUnits().swapWith(ut, assignStmt);
+          break;
+        }
+      }
+    }
+    // Then replace the actual constructor call
+    for (Unit ut : body.getUnits()) {
+      if (ut instanceof InvokeStmt) {
+        InvokeExpr invokeExpr = ((InvokeStmt)ut).getInvokeExpr();
+        boolean classMatch = invokeExpr.getMethod().getDeclaringClass().getName().equals(inputToReplace.getType().toString());
+        boolean methodMatch = invokeExpr.getMethod().getName().equals("<init>");
+        if (classMatch && methodMatch) {
+            body.getUnits().swapWith(ut, constructorInvoke);
+            break;
+        }
+      }
+    }
   }
 
 }
