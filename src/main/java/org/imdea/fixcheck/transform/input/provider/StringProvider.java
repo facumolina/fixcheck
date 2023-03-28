bcb78074650eda65b6aa248a92714ebadb246c14
Facundo Molina <facu.molina7@gmail.com>
2023-03-28 11:54:50 +0200

    String provider randomly selects strings from existing constants if present in the test under analysis

--- src/main/java/org/imdea/fixcheck/transform/input/provider/StringProvider.java
+++ src/main/java/org/imdea/fixcheck/transform/input/provider/StringProvider.java
@@ -5,6 +5,7 @@ import soot.jimple.StringConstant;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /**
  * String Provider class: provides strings for the input transformer.
@@ -12,15 +13,24 @@ import java.util.List;
 public class StringProvider implements InputProvider {
 
   List<Value> usedInputs = new ArrayList<>();
+  Random random = new Random();
+
+  public StringProvider() {
+    usedInputs.add(StringConstant.v("maxspeed:forward"));
+  }
 
   @Override
   public Value getInput() {
+    if (usedInputs.size() > 0) {
+      int i = random.nextInt(usedInputs.size());
+      return usedInputs.get(i);
+    }
     return StringConstant.v("test");
   }
 
   @Override
   public void addInput(Value value) {
-    usedInputs.add(value);
+    //usedInputs.add(value);
   }
 
   @Override
