a8f5f1e640a1e54bcadd9cc2d7a585b52aa99e9e
Facundo Molina <facu.molina7@gmail.com>
2023-03-30 15:08:06 +0200

    Variables to keep track of time spent in each phase

--- src/main/java/org/imdea/fixcheck/utils/Stats.java
+++ src/main/java/org/imdea/fixcheck/utils/Stats.java
@@ -12,5 +12,9 @@ public class Stats {
   public static int TOTAL_ASSERTION_FAILING_PREFIXES = 0;
   public static int TOTAL_PASSING_PREFIXES = 0;
 
+  // Time data
+  public static long MS_PREFIXES_GENERATION = 0;
+  public static long MS_ASSERTIONS_GENERATION = 0;
+  public static long MS_RUNNING_PREFIXES = 0;
 
 }
