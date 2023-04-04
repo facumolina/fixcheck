fcf9fc2155a3545b3d57f6e8d4a65faebb21db40
Facundo Molina <facu.molina7@gmail.com>
2023-04-04 17:59:33 +0200

    Fix script obtaining bad fixes csv

--- experiments/get-badfixes.py
+++ experiments/get-badfixes.py
@@ -19,7 +19,7 @@ all_bad_fixes = sorted(os.listdir(subjects_folder))
 all_jsons_files = [subjects_folder + bad_fix + '/bad-fix.json' for bad_fix in all_bad_fixes]
 all_bad_fixes_json = [json.load(open(bad_fix_json_file)) for bad_fix_json_file in all_jsons_files]
 
-headers = ['id', 'project', 'main_dep', 'tests_classes', 'target_test', 'test_src_dir', 'target_class', 'input_class']
+headers = ['id', 'project', 'main_dep', 'tests_classes', 'test_src_dir', 'target_test', 'target_test_methods', 'target_class', 'input_class']
 
 bad_fixes_rows = []
 for bad_fix_json in all_bad_fixes_json:
@@ -39,7 +39,8 @@ for bad_fix_json in all_bad_fixes_json:
         bad_fix_target_test = bad_fix_data['test_class']
         bad_fix_target_class = ''
         bad_fix_input_class = ''
-        bad_fix_row = [bad_fix_name, bad_fix_project, bad_fix_main_dep, bad_fix_tests_classes, bad_fix_target_test, bad_fix_test_src_dir, bad_fix_target_class, bad_fix_input_class]
+        bad_fix_target_methods = ''
+        bad_fix_row = [bad_fix_name, bad_fix_project, bad_fix_main_dep, bad_fix_tests_classes, bad_fix_test_src_dir, bad_fix_target_test, bad_fix_target_methods,bad_fix_target_class, bad_fix_input_class]
         bad_fixes_rows.append(bad_fix_row)
     else:
         print(f'Skipping bad fix without test class: {bad_fix_name}')
