83d8ca12b2d8fab9fd917aaa8f4f1a2d773c7e14
Facundo Molina <facu.molina7@gmail.com>
2023-04-05 19:18:48 +0200

    Just preserving the TOGA assertion generator base class

---
+++ src/main/java/org/imdea/fixcheck/assertion/TOGAAssertionGenerator.java
@@ -0,0 +1,16 @@
+package org.imdea.fixcheck.assertion;
+
+import org.imdea.fixcheck.prefix.Prefix;
+
+/**
+ * TOGAAssertionGenerator class: generator the obtains assertions using the TOGA model (see <a href="https://github.com/microsoft/toga">TOGA</a>).
+ * @author Facundo Molina
+ */
+public class TOGAAssertionGenerator extends AssertionGenerator {
+
+  @Override
+  public void generateAssertions(Prefix prefix) {
+
+  }
+
+}
