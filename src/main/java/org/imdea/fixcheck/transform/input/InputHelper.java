a4456a127fe38db87409eed9b2ace579c8435c8d
Facundo Molina <facu.molina7@gmail.com>
2023-04-06 16:40:55 +0200

    InputHelper now using JavaParser classes

--- src/main/java/org/imdea/fixcheck/transform/input/InputHelper.java
+++ src/main/java/org/imdea/fixcheck/transform/input/InputHelper.java
@@ -1,9 +1,9 @@
 package org.imdea.fixcheck.transform.input;
 
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.expr.*;
 import org.imdea.fixcheck.transform.input.provider.*;
-import org.imdea.fixcheck.Properties;
 import soot.*;
-import soot.jimple.Constant;
 
 import java.util.*;
 
@@ -13,29 +13,27 @@ import java.util.*;
  */
 public class InputHelper {
 
-  protected static Map<String, List<Class<?>>> INPUTS_BY_TYPE; // Map of possible input classes for each type
-  public static Map<Class<?>, InputProvider> PROVIDERS;
+  protected static Map<String, List<Class<? extends Expression>>> INPUTS_BY_TYPE; // Map of possible input classes for each type
+  public static Map<Class<? extends Expression>, InputProvider> PROVIDERS;
   static {
     PROVIDERS = new HashMap<>();
-    PROVIDERS.put(int.class, new IntegerProvider());
-    PROVIDERS.put(java.lang.Integer.class, new IntegerProvider());
-    PROVIDERS.put(boolean.class, new BooleanProvider());
-    PROVIDERS.put(java.lang.Boolean.class, new BooleanProvider());
-    PROVIDERS.put(java.lang.Long.class, new LongProvider());
-    PROVIDERS.put(java.lang.String.class, new StringProvider());
+    PROVIDERS.put(IntegerLiteralExpr.class, new IntegerProvider());
+    PROVIDERS.put(BooleanLiteralExpr.class, new BooleanProvider());
+    PROVIDERS.put(LongLiteralExpr.class, new LongProvider());
+    PROVIDERS.put(StringLiteralExpr.class, new StringProvider());
   }
 
   public static void initializeHelper() {
     // Initialize the map of inputs by type
     INPUTS_BY_TYPE = new HashMap<>();
-    INPUTS_BY_TYPE.put("java.util.Boolean", Collections.singletonList(Boolean.class));
-    INPUTS_BY_TYPE.put("int", Collections.singletonList(int.class));
-    INPUTS_BY_TYPE.put("java.lang.Integer", Collections.singletonList(Integer.class));
-    INPUTS_BY_TYPE.put("java.lang.Long", Collections.singletonList(Long.class));
-    INPUTS_BY_TYPE.put("java.lang.Float", Collections.singletonList(Float.class));
-    INPUTS_BY_TYPE.put("java.lang.Double", Collections.singletonList(Double.class));
-    INPUTS_BY_TYPE.put("java.lang.String", Collections.singletonList(String.class));
-    INPUTS_BY_TYPE.put("java.lang.Object", Arrays.asList(Boolean.class, Integer.class, Long.class, String.class));
+    INPUTS_BY_TYPE.put("java.util.Boolean", Collections.singletonList(BooleanLiteralExpr.class));
+    INPUTS_BY_TYPE.put("int", Collections.singletonList(IntegerLiteralExpr.class));
+    INPUTS_BY_TYPE.put("java.lang.Integer", Collections.singletonList(IntegerLiteralExpr.class));
+    INPUTS_BY_TYPE.put("java.lang.Long", Collections.singletonList(LongLiteralExpr.class));
+    //INPUTS_BY_TYPE.put("java.lang.Float", Collections.singletonList(FloatLi));
+    INPUTS_BY_TYPE.put("java.lang.Double", Collections.singletonList(DoubleLiteralExpr.class));
+    INPUTS_BY_TYPE.put("java.lang.String", Collections.singletonList(StringLiteralExpr.class));
+    INPUTS_BY_TYPE.put("java.lang.Object", Arrays.asList(BooleanLiteralExpr.class, IntegerLiteralExpr.class, LongLiteralExpr.class, StringLiteralExpr.class));
     // Feed providers with inputs that can be obtained from the test suite under analysis
     searchForInputs();
   }
@@ -88,7 +86,7 @@ public class InputHelper {
    * @param type Type of the input
    * @return Value for the given type
    */
-  public static Value getValueForType(Class<?> type) {
+  public static Object getValueForType(Class<? extends Expression> type) {
     if (PROVIDERS.containsKey(type)) {
       InputProvider provider = PROVIDERS.get(type);
       return provider.getInput();
