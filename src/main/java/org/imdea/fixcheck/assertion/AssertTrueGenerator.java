24665d7c39f69f27b928fee0bfa71cdcc6a79314
Facundo Molina <facu.molina7@gmail.com>
2023-04-10 12:00:14 +0200

    Fixing AssertTrueGenerator

--- src/main/java/org/imdea/fixcheck/assertion/AssertTrueGenerator.java
+++ src/main/java/org/imdea/fixcheck/assertion/AssertTrueGenerator.java
@@ -1,10 +1,7 @@
 package org.imdea.fixcheck.assertion;
 
-import org.imdea.fixcheck.assertion.common.AssertionsHelper;
+import com.github.javaparser.ast.body.MethodDeclaration;
 import org.imdea.fixcheck.prefix.Prefix;
-import soot.*;
-import soot.jimple.IntConstant;
-import soot.jimple.Jimple;
 
 /**
  * AssertTrueGenerator class: simple generator including an assert(true) at the end of the prefix, just for testing.
@@ -15,19 +12,9 @@ public class AssertTrueGenerator extends AssertionGenerator {
     @Override
     public void generateAssertions(Prefix prefix) {
       // Take the method and units
-      /*SootMethod method = prefix.getMethod();
-      Body body = method.retrieveActiveBody();
-      UnitPatchingChain units = body.getUnits();
-      // Define local for org.junit.Assert
-      Local assertRef = Jimple.v().newLocal("assertRef", RefType.v("org.junit.Assert"));
-      body.getLocals().add(assertRef);
+      MethodDeclaration method = prefix.getMethod();
       // Add the assertTrue at the end of the prefix
-      SootMethod assertMethod = AssertionsHelper.getAssertTrueMethod();
-      Unit unit = Jimple.v().newInvokeStmt(Jimple.v().newStaticInvokeExpr(assertMethod.makeRef(), IntConstant.v(1)));
-      units.removeLast(); // Remove the return statement
-      units.addLast(unit);
-      units.addLast(Jimple.v().newReturnVoidStmt());*/
-      System.out.println("FIX ME: AssertTrueGenerator.generateAssertions");
+      method.getBody().get().addStatement("assertTrue(true);");
     }
 
 }
