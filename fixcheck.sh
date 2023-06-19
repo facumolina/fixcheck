60f11e842b7757741e96a04b4dbb1c48c9438291
Facundo Molina <facu.molina7@gmail.com>
2023-06-19 09:01:22 +0200

    Removing comments

--- fixcheck.sh
+++ fixcheck.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-#subject_cp='build/libs/fixcheck-all-1.0.0.jar:/Users/facundo.molina/research/software/bad-fixes-dataset/tmp/jackson-databind-118/badfix/jackson-databind/target/jackson-databind-2.1.2-SNAPSHOT-jar-with-dependencies.jar:/Users/facundo.molina/research/software/bad-fixes-dataset/tmp/jackson-databind-118/badfix/jackson-databind/target/test-classes'
-#target_test='com.fasterxml.jackson.databind.jsontype.TestExternalIdForAnalysis'
-#target_class='com.fasterxml.jackson.databind.ObjectMapper'
-#inputs_class='java.util.Date'
-
 subject_cp=$1
 test_classes_path=$2
 target_test=$3
