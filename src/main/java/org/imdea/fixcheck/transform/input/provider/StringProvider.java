8407261df946d2247b8be44bbb93cfcc650b23bf
Facundo Molina <facu.molina7@gmail.com>
2023-06-20 14:57:36 +0200

    Sometimes, use random char as part of input strings

--- src/main/java/org/imdea/fixcheck/transform/input/provider/StringProvider.java
+++ src/main/java/org/imdea/fixcheck/transform/input/provider/StringProvider.java
@@ -12,13 +12,22 @@ public class StringProvider implements InputProvider {
   List<Object> usedInputs = new ArrayList<>();
   Random random = new Random();
 
+  private static final String specialChars = "!@#$%^&*()_+-=[]{};':\",./<>?\\|`~";
+
   @Override
   public Object getInput() {
+    String input = "\"test\"";
     if (usedInputs.size() > 0) {
       int i = random.nextInt(usedInputs.size());
-      return usedInputs.get(i);
+      input = (String)usedInputs.get(i);
     }
-    return "\"test\"";
+    // Sometimes, include a special character
+    if (random.nextInt(5) == 0) {
+      int j = random.nextInt(input.length());
+      char randomChar = specialChars.charAt(random.nextInt(specialChars.length()));
+      input = input.substring(0, j) + randomChar + input.substring(j);
+    }
+    return input;
   }
 
   @Override
