fd85ce1089942add3659c59ee16fc1cc29ad376c
Facundo Molina <facu.molina7@gmail.com>
2023-06-28 11:30:00 +0200

    Adding script to setup all defects repairing subjects

---
+++ experiments/setup-all-defect-repairing.py
@@ -0,0 +1,12 @@
+import sys
+import pandas as pd
+import subprocess
+
+target_ids = [1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 36, 37, 38, 44, 45, 46, 47, 48, 49, 51, 53, 54, 55, 58, 59, 62, 63, 64, 65, 66, 67, 68, 69, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 88, 89, 90, 91, 92, 93, 150, 151, 152, 153, 154, 155, 157, 158, 159, 160, 161, 162, 163, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 191, 192, 193, 194, 195, 196, 197, 198, 199, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 'HDRepair1', 'HDRepair3', 'HDRepair4', 'HDRepair5', 'HDRepair6', 'HDRepair7', 'HDRepair8', 'HDRepair9', 'HDRepair10']
+# Append each id to the word Patch
+target_patches = ['Patch' + str(id) for id in target_ids]
+
+# Run the script setup-defect-repairing.py for each patch
+for patch in target_patches:
+    print(f'Running setup for patch: {patch}')
+    subprocess.run(f'python3 experiments/setup-defect-repairing.py {patch}', shell=True)
