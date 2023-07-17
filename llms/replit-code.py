ab506b64177e25e1bfc9e335d5e5519bd1f06def
Facundo Molina <facu.molina7@gmail.com>
2023-07-17 16:08:06 +0200

    Adding replit code llm script from transformers

---
+++ llms/replit-code.py
@@ -0,0 +1,32 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import flask
+
+tokenizer = AutoTokenizer.from_pretrained('replit/replit-code-v1-3b', trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained('replit/replit-code-v1-3b', trust_remote_code=True)
+
+x = tokenizer.encode('def fibonacci(n): ', return_tensors='pt')
+y = model.generate(x, max_length=100, do_sample=True, top_p=0.95, top_k=4, temperature=0.2, num_return_sequences=1, eos_token_id=tokenizer.eos_token_id)
+
+# decoding, clean_up_tokenization_spaces=False to ensure syntactical correctness
+generated_code = tokenizer.decode(y[0], skip_special_tokens=True, clean_up_tokenization_spaces=False)
+print(generated_code)
+
+
+# Run flask server
+app = flask.Flask(__name__)
+
+@app.route('/complete', methods=['POST'])
+def complete():
+    prompt = flask.request.json['prompt']
+    print("### Prompt:")
+    x = tokenizer.encode(prompt, return_tensors='pt')
+    y = model.generate(x, max_length=48, temperature=0.2, top_p=0.9, top_k=4, num_return_sequences=1, eos_token_id=tokenizer.eos_token_id)
+    print("### Model output:")
+    output = tokenizer.decode(y[0], skip_special_tokens=True, clean_up_tokenization_spaces=False)
+    print(output)
+    return flask.jsonify({'completion': output})
+
+# Run the app
+port = 5100
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=port)
