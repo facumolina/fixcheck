6a9cf59e088d722e6c8df762d7e56faeade94afd
Facundo Molina <facu.molina7@gmail.com>
2023-07-17 16:17:57 +0200

    Avoid original call in model

--- llms/replit-code.py
+++ llms/replit-code.py
@@ -4,13 +4,12 @@ import flask
 tokenizer = AutoTokenizer.from_pretrained('replit/replit-code-v1-3b', trust_remote_code=True)
 model = AutoModelForCausalLM.from_pretrained('replit/replit-code-v1-3b', trust_remote_code=True)
 
-x = tokenizer.encode('def fibonacci(n): ', return_tensors='pt')
-y = model.generate(x, max_length=100, do_sample=True, top_p=0.95, top_k=4, temperature=0.2, num_return_sequences=1, eos_token_id=tokenizer.eos_token_id)
+#x = tokenizer.encode('def fibonacci(n): ', return_tensors='pt')
+#y = model.generate(x, max_length=100, do_sample=True, top_p=0.95, top_k=4, temperature=0.2, num_return_sequences=1, eos_token_id=tokenizer.eos_token_id)
 
 # decoding, clean_up_tokenization_spaces=False to ensure syntactical correctness
-generated_code = tokenizer.decode(y[0], skip_special_tokens=True, clean_up_tokenization_spaces=False)
-print(generated_code)
-
+#generated_code = tokenizer.decode(y[0], skip_special_tokens=True, clean_up_tokenization_spaces=False)
+#print(generated_code)
 
 # Run flask server
 app = flask.Flask(__name__)
