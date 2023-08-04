9c68dd70f55927d92dd3f68d0858d2b3982f5ef3
Facundo Molina <facu.molina7@gmail.com>
2023-08-04 16:30:52 +0200

    FixCheck by default generates 100 tests

--- fixcheck.sh
+++ fixcheck.sh
@@ -11,4 +11,4 @@ original_failure_log=$8
 assertion_generation=$9
 full_cp='build/libs/fixcheck-all-1.0.0.jar:'$subject_cp
 
-java -cp $full_cp org.imdea.fixcheck.FixCheck $test_classes_path $target_test $target_test_methods $target_test_dir $target_class $inputs_class $original_failure_log 10 $assertion_generation | tee log.out
+java -cp $full_cp org.imdea.fixcheck.FixCheck $test_classes_path $target_test $target_test_methods $target_test_dir $target_class $inputs_class $original_failure_log 100 $assertion_generation | tee log.out
