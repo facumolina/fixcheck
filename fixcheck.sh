861c732cc3d9b4f0d0b667a0d35f9a9851bff3fb
Facundo Molina <facu.molina7@gmail.com>
2024-06-28 16:09:30 +0200

    update script to pass the arguments properly

--- fixcheck.sh
+++ fixcheck.sh
@@ -11,4 +11,4 @@ original_failure_log=$8
 assertion_generation=$9
 full_cp='build/libs/fixcheck-all-1.0.0.jar:'$subject_cp
 
-java -cp $full_cp org.imdea.fixcheck.FixCheck $test_classes_path $target_test $target_test_methods $target_test_dir $target_class $inputs_class $original_failure_log 100 $assertion_generation | tee log.out
+java -cp $full_cp org.imdea.fixcheck.FixCheck -tp $test_classes_path -tc $target_test -tm $target_test_methods -ts $target_test_dir -i $inputs_class -tf $original_failure_log -np 100 -ag $assertion_generation | tee log.out
