7a57466917dff4d0c9d7b9e0b639d35fc25ba379
Facundo Molina <facu.molina7@gmail.com>
2024-07-04 09:51:03 +0200

    printing command

--- fixcheck.sh
+++ fixcheck.sh
@@ -11,4 +11,7 @@ original_failure_log=$8
 assertion_generation=$9
 full_cp='build/libs/fixcheck-all-1.0.0.jar:'$subject_cp
 
+cmd="java -cp $full_cp org.imdea.fixcheck.FixCheck -tp $test_classes_path -tc $target_test -tm $target_test_methods -ts $target_test_dir -i $inputs_class -tf $original_failure_log -np 100 -ag $assertion_generation"
+echo 'command: '$cmd
+echo ''
 java -cp $full_cp org.imdea.fixcheck.FixCheck -tp $test_classes_path -tc $target_test -tm $target_test_methods -ts $target_test_dir -i $inputs_class -tf $original_failure_log -np 100 -ag $assertion_generation | tee log.out
