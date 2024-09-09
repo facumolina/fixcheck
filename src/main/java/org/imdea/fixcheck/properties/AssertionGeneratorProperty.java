1ce9ded75743360ac62ff95d9756e3beef7521fa
Facundo Molina <facu.molina7@gmail.com>
2024-09-09 11:27:44 +0200

    handling properly other llms

--- src/main/java/org/imdea/fixcheck/properties/AssertionGeneratorProperty.java
+++ src/main/java/org/imdea/fixcheck/properties/AssertionGeneratorProperty.java
@@ -18,16 +18,22 @@ public class AssertionGeneratorProperty {
     map.put("assert-true", "org.imdea.fixcheck.assertion.AssertTrueGenerator");
     map.put("previous-assertion", "org.imdea.fixcheck.assertion.UsePreviousAssertGenerator");
     map.put("replit-code-llm", "org.imdea.fixcheck.assertion.ReplitCodeLLM");
-    map.put("gpt-3.5", "org.imdea.fixcheck.assertion.GPT35Turbo");
-    map.put("ollama", "org.imdea.fixcheck.assertion.OllamaAssertionGenerator");
+    map.put("gpt-3.5", "org.imdea.fixcheck.assertion.GPT3_5Turbo");
+    map.put("codellama", "org.imdea.fixcheck.assertion.CodeLLamaOllama");
+    map.put("llama3.1", "org.imdea.fixcheck.assertion.Llama3_1Ollama");
     return map;
   }
 
   public static String parseOption(String option) {
     if (!Options.containsKey(option)) {
-      System.out.println("Error: Invalid option for assertion generator: " + option);
-      System.out.println("Valid options are: " + Options.keySet());
-      System.exit(1);
+      if (option.startsWith("ollama-")) {
+        String ollamaOption = option.split("-")[1];
+        return "org.imdea.fixcheck.assertion.OllamaAssertionGenerator#" + ollamaOption;
+      } else {
+        System.out.println("Error: Invalid option for assertion generator: " + option);
+        System.out.println("Valid options are: " + Options.keySet());
+        System.exit(1);
+      }
     }
     return Options.get(option);
   }
